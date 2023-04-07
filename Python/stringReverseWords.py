c55122ee099893375cd3d817cfa1a718cb156203
arihant1602 <arihantpratapsingh368@gmail.com>
2023-04-07 21:48:23 +0530

    bing

---
+++ Python/stringReverseWords.py
@@ -0,0 +1,15 @@
+def reverseWords(str):
+    new = []
+    print(str.split())
+#    str = str.strip() <= space hatata hai
+    for i in str.split():
+        print(i)
+        i = (i.lower())[:-1] + (i[-1].upper())
+        new.append(i[::-1])
+    return (" ".join(new))
+
+print(reverseWords("Hello and my name is Arihant"))
+
+string = "Hello and my name is Arihant"
+string = string.replace("my", "your")
+print(string)
